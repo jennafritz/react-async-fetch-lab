85a7c66d3ebfda6278963501b2597f17f0644e3f
jennafritz <jennafritz24@gmail.com>
2021-04-29 12:46:32 -0400

    commit tree 17708787e2e15f7644e58e6a054c644c2fdab627

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import {Component} from 'react'
+import React from 'react'
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            people: []
+        }
+    }
+    
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res => res.json())
+            .then(data => this.setState({people: data.people}))
+    }
+
+
+    render(){
+        console.log(this.state.people)
+        return(
+            <ul>
+                {this.state.people.map(person => {
+                    return <li>{person.name}</li>
+                })}
+            </ul>
+        )
+    }
+}
+
+export default App
